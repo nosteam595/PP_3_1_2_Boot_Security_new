cf72b96faada6b13aa8421d126d6731385729dba
nosteam595 <nosteam@bk.ru>
2026-03-05 22:47:33 +0300

    Initial commit

---
+++ src/main/java/nosteam/IdeaProjects/PP_3_1_2_Boot_Security_new/Pp312BootSecurityNewApplication.java
@@ -0,0 +1,13 @@
+package nosteam.IdeaProjects.PP_3_1_2_Boot_Security_new;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pp312BootSecurityNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pp312BootSecurityNewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nosteam/IdeaProjects/PP_3_1_2_Boot_Security_new/Pp312BootSecurityNewApplicationTests.java
@@ -0,0 +1,13 @@
+package nosteam.IdeaProjects.PP_3_1_2_Boot_Security_new;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pp312BootSecurityNewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
