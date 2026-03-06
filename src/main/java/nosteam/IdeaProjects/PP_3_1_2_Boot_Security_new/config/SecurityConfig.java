cfa540762f48a254d6db079a531d154af99b6c3c
nosteam595 <nosteam@bk.ru>
2026-03-06 23:58:19 +0300

    custom auth

--- src/main/java/nosteam/IdeaProjects/PP_3_1_2_Boot_Security_new/config/SecurityConfig.java
+++ src/main/java/nosteam/IdeaProjects/PP_3_1_2_Boot_Security_new/config/SecurityConfig.java
@@ -1,65 +1,50 @@
 package nosteam.IdeaProjects.PP_3_1_2_Boot_Security_new.config;
 
-import nosteam.IdeaProjects.PP_3_1_2_Boot_Security_new.security.AuthProviderImpl;
+import nosteam.IdeaProjects.PP_3_1_2_Boot_Security_new.services.PersonDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        // Разрешаем только страницу ошибки, так как /login теперь встроенный
-                        .requestMatchers("/error").permitAll()
+                        .requestMatchers("/auth/login", "/error").permitAll()
                         .anyRequest().authenticated()
                 )
-                .authenticationProvider(authProvider)
-
                 .formLogin(form -> form
-                        // .loginPage() НЕ ПИШЕМ — тогда включится стандартная форма
-                        .defaultSuccessUrl("/hello", true) // Куда идти после успеха
+                        .loginPage("/auth/login")
+                        .loginProcessingUrl("/process_login")
+                        .defaultSuccessUrl("/hello", true)
                         .permitAll()
                 )
                 .logout(logout -> logout
-                        .logoutSuccessUrl("/login?logout") // Стандартный URL после выхода
-                );
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/auth/login")
+                )
+                .userDetailsService(personDetailsService);
 
         return http.build();
     }
-//Для кастомной страницы авторизации
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .csrf(csrf -> csrf.disable())
-//                .authorizeHttpRequests(auth -> auth
-//                        // ВАЖНО: путь должен СОВПАДАТЬ с контроллером (включая /auth)
-//                        .requestMatchers("/auth/login", "/process_login", "/error").permitAll()
-//                        .anyRequest().authenticated()
-//                )
-//                .formLogin(form -> form
-//                        .loginPage("/auth/login") // Страница, которую мы разрешили выше
-//                        .loginProcessingUrl("/process_login") // Скрытый URL, который обрабатывает POST-данные
-//                        .defaultSuccessUrl("/hello", true)
-//                        .permitAll() // Добавьте это для надежности
-//                )
-//                .logout(logout -> logout
-//                        .logoutUrl("/logout")
-//                        .logoutSuccessUrl("/auth/login")
-//                );
-//
-//        return http.build();
-//    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
 
